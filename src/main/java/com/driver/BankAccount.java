c0b22fcf62276b49213c3e2bd2a1d83ab6110cf3
Pankaj1656 <pc69269@gmail.com>
2023-05-26 09:23:32 +0530

    fourth

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -1,10 +1,5 @@
 package com.driver;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class BankAccount {
 
     private String name;
@@ -12,49 +7,67 @@ public class BankAccount {
     private double minBalance;
 
     public BankAccount(String name, double balance, double minBalance) {
-         this.name=name;
-         this.balance=balance;
-         this.minBalance=minBalance;
+        this.name = name;
+        this.balance = balance;
+        this.minBalance = minBalance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public double getMinBalance() {
+        return minBalance;
     }
 
     public String generateAccountNumber(int digits, int sum) throws Exception{
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
-        if(digits*9 <sum){
-            throw new Exception("Account Number can not be generated exception");
+
+        int rem = sum;
+        String accNo = "";
+        if(digits*9 < sum){
+            throw new Exception("Account Number can not be generated");
         }
-        String accNo="";
-        int rem=sum;
-        while(digits>0 && rem >0){
-            if(rem>=9) {
-                accNo += "9";
-                rem = rem - 9;
+        else{
+            while(digits > 0 && rem > 0){
+                if(rem >= 9){
+                    rem = rem - 9;
+                    accNo = accNo + "9";
+                }
+                else{
+                    accNo = accNo + Integer.toString(rem);
+                    rem = 0;
+                }
+                digits--;
             }
-            else{
-                accNo+=Integer.toString(rem);
-                rem=0;
+            while(digits > 0){
+                accNo = accNo + "0";
+                digits--;
             }
-            digits--;
+            return accNo;
         }
-        while(digits>0){
-            accNo+="0";
-            digits--;
-        }
-        return accNo;
     }
 
     public void deposit(double amount) {
         //add amount to balance
-        this.balance+=amount;
+        this.balance += amount;
+
 
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
-        if (this.balance - amount >= getMinBalance()) {
+        if(this.balance - amount >= getMinBalance()){
             this.balance = this.balance - amount;
-        } else throw new Exception("Insufficient Balance");
+        }
+        else throw new Exception("Insufficient Balance");
+
     }
 
 }
